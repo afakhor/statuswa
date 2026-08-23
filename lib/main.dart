6aa81e39f5df922a062887c930fdc9503994c459
afakhor <31812946+afakhor@users.noreply.github.com>
2026-08-23 15:20:05 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'dart:ui' as ui;
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,116 +9,113 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+    return const MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black12,
+        body: Center(child: GlobeShaderWidget()),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class GlobeShaderWidget extends StatefulWidget {
+  const GlobeShaderWidget({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<GlobeShaderWidget> createState() => _GlobeShaderWidgetState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _GlobeShaderWidgetState extends State<GlobeShaderWidget>
+    with SingleTickerProviderStateMixin {
+  ui.FragmentShader? _shader;
+  ui.Image? _textureImage;
+  late AnimationController _controller;
 
-  void _incrementCounter() {
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 10),
+    )..repeat();
+
+    _loadShaderAndTexture();
+  }
+
+  Future<void> _loadShaderAndTexture() async {
+    // 1. Load Fragment Shader
+    final program = await ui.FragmentProgram.fromAsset('shaders/globe.frag');
+    
+    // 2. Load Gambar Tekstur dari Assets
+    final ByteData data = await rootBundle.load('assets/texture.png');
+    final codec = await ui.instantiateImageCodec(data.buffer.asUint8List());
+    final frame = await codec.getNextFrame();
+
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _shader = program.fragmentShader();
+      _textureImage = frame.image;
     });
   }
 
   @override
+  void dispose() {
+    _controller.dispose();
+    _shader?.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+    if (_shader == null || _textureImage == null) {
+      return const CircularProgressIndicator();
+    }
+
+    return AnimatedBuilder(
+      animation: _controller,
+      builder: (context, child) {
+        return CustomPaint(
+          size: const Size(350, 350),
+          painter: GlobePainter(
+            shader: _shader!,
+            texture: _textureImage!,
+            time: _controller.value * 2 * 3.14159, // Rotasi 360 derajat
+          ),
+        );
+      },
     );
   }
+}
+
+class GlobePainter extends CustomPainter {
+  final ui.FragmentShader shader;
+  final ui.Image texture;
+  final double time;
+
+  GlobePainter({
+    required this.shader,
+    required this.texture,
+    required this.time,
+  });
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    // Set Uniforms untuk Shader GLSL:
+    // Index 0: uResolution.x
+    // Index 1: uResolution.y
+    // Index 2: uTime
+    shader.setFloat(0, size.width);
+    shader.setFloat(1, size.height);
+    shader.setFloat(2, time);
+
+    // Set Sampler0: uTexture (Gambar Tekstur)
+    shader.setImageSampler(0, texture);
+
+    final paint = Paint()..shader = shader;
+    canvas.drawRect(Rect.fromLTWH(0, 0, size.width, size.height), paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant GlobePainter oldDelegate) => true;
 }
